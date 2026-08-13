b936a884ab67ba41aad91368854b637e685f2aa3
Frau Yang <53266424+ichbinfrauyang@users.noreply.github.com>
2026-08-13 14:51:52 +0800

    Fix image upload path

--- server/middlewares/upload.js
+++ server/middlewares/upload.js
@@ -1,13 +1,43 @@
+// import multer from "multer";
+// const storage = multer.diskStorage({
+//     destination:(req,file,cb)=>{
+//         cb(null,"uploads/");
+//     },
+
+//     filename:(req,file,cb)=>{
+//         cb(
+//             null,
+//             Date.now()+"-"+file.originalname
+//         );
+//     }
+// });
+
+// const upload = multer({
+//     storage
+// });
+
+// export default upload;
+
 import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadDir = path.join(process.cwd(), "uploads");
+
+// 如果 uploads 不存在，就创建
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
+
 const storage = multer.diskStorage({
-    destination:(req,file,cb)=>{
-        cb(null,"uploads/");
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
     },
 
-    filename:(req,file,cb)=>{
+    filename: (req, file, cb) => {
         cb(
             null,
-            Date.now()+"-"+file.originalname
+            Date.now() + "-" + file.originalname
         );
     }
 });
